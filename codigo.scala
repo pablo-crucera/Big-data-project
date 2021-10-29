71e3969fb1ca2b4d15bf54eb69e477a94bb12e0b
Julia-upc <juliasanchezmartinez98@gmail.com>
2021-10-29 12:00:57 +0200

    Update codigo.scala

--- codigo.scala
+++ codigo.scala
@@ -1,2 +1,12 @@
+// IMPORT AND LOAD DATASET
 
-df = spark.read.format("csv").option("header", "true").load("~/Escritorio/Big-data-project/datos big data/2008.csv")
+val df = spark.read.format("csv").option("header", "true").load("/home/julia/Escritorio/Big-data-project/datosbigdata/2008.csv")
+
+
+// PREPROCESS DATA
+
+import spark.implicits._
+import org.apache.spark.sql.functions.{col, column, expr}
+val r=df.select("Year", "Month", "DayofMonth", "DayOfWeek", "DepTime", "CRSDepTime", "CRSArrTime", "UniqueCarrier", "FlightNum", "TailNum", "CRSElapsedTime", "ArrDelay", "DepDelay", "Origin", "Dest", "Distance", "TaxiOut", "Cancelled", "CancellationCode")
+
+
